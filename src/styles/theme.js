7960d0e95a40172790cde0e26dd9fa37178cd938
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-01-29 15:01:53 +0100

    add global styles, theme

---
+++ src/styles/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  colors: {
+    dark: 'hsl(229, 25%, 31%)',
+    blue: 'hsl(229, 64%, 46%)',
+    grey: 'hsl(217, 16%, 45%)',
+  },
+  gradients: {
+    yellow: 'hsl(39, 89%, 49%) to hsl(40, 84%, 53%)',
+    blue: 'hsl(230, 89%, 62%) to hsl(230, 89%, 65%)',
+    red: 'hsl(349, 71%, 52%) to hsl(349, 70%, 56%)',
+    purple: 'hsl(261, 73%, 60%) to hsl(261, 72%, 63%)',
+    cyan: 'hsl(189, 59%, 53%) to hsl(189, 58%, 57%)',
+    darkBlue: 'hsl(214, 47%, 23%) to hsl(237, 49%, 15%)',
+  },
+  layout: {
+    mobile: '375px',
+    desktop: '1366px',
+  },
+  fontWeight: {
+    regular: 700,
+    bold: 800,
+  },
+  fontFamily: 'Barlow Semi Condensed',
+  dimensions: { height: { regular: '19.4rem' }, width: { regular: '19.8rem' } },
+};
+
+export default theme;
